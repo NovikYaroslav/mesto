4692b1c5917bf1629d5c527a598f9fb920553885
Yaroslav Novik <yaroslavnowik@yandex.ru>
2022-09-17 19:53:02 +0300

    final revision

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,26 @@
-// получить элементы
-const PopupOpenButton = document.querySelector(".edit-button");
-const Popup = document.querySelector(".popup");
-const PopupCloseButton = Popup.querySelector(".popup__close");
-const PopupSaveButton = Popup.querySelector(".popup__save");
-let formElement = Popup.querySelector(".user-discription");
-let nameInput = formElement.querySelector(".user-discription__name");
-let aboutInput = formElement.querySelector(".user-discription__about");
+const popupOpenButton = document.querySelector(".edit-button");
+const popup = document.querySelector(".popup");
+const popupCloseButton = popup.querySelector(".popup__close");
+const popupSaveButton = popup.querySelector(".popup__save");
+const formElement = popup.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".user-discription__name");
+const aboutInput = formElement.querySelector(".user-discription__about");
+const userName = document.querySelector(".profile-info__user-name");
+const aboutUser = document.querySelector(".profile-info__user-discription");
 
-const UserName = document.querySelector(".profile-info__user-name")
-const AboutUser = document.querySelector(".profile-info__user-discription")
-
-const OpenPopup = function () {
-  Popup.classList.add("popup_opened");
-};
-const ClosePopup = function () {
-    Popup.classList.remove("popup_opened");
-  };
-
-const SavePopup = function (evt) {
-    evt.preventDefault();
-    // nameInput.value = UserName.textContent;
-    // aboutInput.value = AboutUser.textContent;
-    UserName.textContent = nameInput.value;
-    AboutUser.textContent = aboutInput.value;
-    ClosePopup()
+function openPopup() {
+  popup.classList.add("popup_opened");
+}
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+function savePopup(evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  aboutUser.textContent = aboutInput.value;
+  closePopup();
 }
 
-
-// добавить слушатель на кнопки 
-PopupOpenButton.addEventListener("click", OpenPopup);
-PopupCloseButton.addEventListener("click", ClosePopup);
-formElement.addEventListener('submit', SavePopup); 
-
-console.log(AboutUser.textContent)
-console.log(UserName.textContent)
-console.log(typeof nameInput.value)
-console.log(aboutInput.value)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// UserName.textContent = " "
-// AboutUser.textContent =" "
-// UserName.textContent = `${nameInput.value}`
-// AboutUser.textContent = `${aboutInput.value}`
+popupOpenButton.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", savePopup);
