f142b585796125faf1d67ffb6a4b6c4ae751f739
Yaroslav Novik <yaroslavnowik@yandex.ru>
2022-09-28 19:47:57 +0300

    feat: html: card popup added; card teamplate added; element delete button added. js: open of card popup added

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,47 @@
-const popupOpenButton = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const popupCloseButton = popup.querySelector(".popup__close");
-const formElement = popup.querySelector(".popup__form");
+const userPopupOpenButton = document.querySelector(".profile__edit-button");
+const cardPopupOpenButton = document.querySelector(".profile__add-button");
+
+const userPopup = document.getElementById("userPopup");
+const cardPopup = document.getElementById("cardPopup");
+
+const userPopupCloseButton = document.getElementById("userPopupClose");
+const cardPopupCloseButton = document.getElementById("cardPopupClose");
+
+
+const formElement = userPopup.querySelector(".popup__form");
 const nameInput = formElement.querySelector(".user-discription__input_value_name");
 const aboutInput = formElement.querySelector(".user-discription__input_value_about");
 const userName = document.querySelector(".profile__name");
 const aboutUser = document.querySelector(".profile__discription");
 
-function openPopup() {
-  popup.classList.add("popup_opened");
+function openUserPopup() {
+  userPopup.classList.add("popup_opened");
   nameInput.value = userName.textContent;
   aboutInput.value = aboutUser.textContent;
 }
-function closePopup() {
-  popup.classList.remove("popup_opened");
+function openCardPopup() {
+  cardPopup.classList.add("popup_opened");
 }
+
+function closeUserPopup() {
+  userPopup.classList.remove("popup_opened");
+}
+function closeCardPopup() {
+  cardPopup.classList.remove("popup_opened");
+}
+//сделать eventTarget
+
+
+
 function savePopup(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   aboutUser.textContent = aboutInput.value;
-  closePopup();
+  closeUserPopup();
 }
 
-popupOpenButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", closePopup);
+userPopupOpenButton.addEventListener("click", openUserPopup);
+cardPopupOpenButton.addEventListener("click", openCardPopup);
+userPopupCloseButton.addEventListener("click", closeUserPopup);
+cardPopupCloseButton.addEventListener("click", closeCardPopup);
 formElement.addEventListener("submit", savePopup);
