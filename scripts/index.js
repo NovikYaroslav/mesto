c8435159c0508c1612e3ad66098bdadb01a7a9de
Yaroslav Novik <yaroslavnowik@yandex.ru>
2022-09-29 18:45:38 +0300

    feat: activation of like buttons added

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,15 @@ const cardPopup = document.getElementById("cardPopup");
 const userPopupCloseButton = document.getElementById("userPopupClose");
 const cardPopupCloseButton = document.getElementById("cardPopupClose");
 
+const LikeButtons = document.querySelectorAll(".element__like-button")
+
 
 const formElement = userPopup.querySelector(".popup__form");
 const nameInput = formElement.querySelector(".user-discription__input_value_name");
 const aboutInput = formElement.querySelector(".user-discription__input_value_about");
 const userName = document.querySelector(".profile__name");
 const aboutUser = document.querySelector(".profile__discription");
+
 
 function openUserPopup() {
   userPopup.classList.add("popup_opened");
@@ -31,8 +34,6 @@ function closeCardPopup() {
 }
 //сделать eventTarget
 
-
-
 function savePopup(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
@@ -40,8 +41,18 @@ function savePopup(evt) {
   closeUserPopup();
 }
 
+function likeCard(event) {
+  event.target.classList.toggle("element__like-button_active")
+}
+
+LikeButtons.forEach(function (likeButton) {
+  likeButton.addEventListener("click", likeCard)
+})
+
 userPopupOpenButton.addEventListener("click", openUserPopup);
 cardPopupOpenButton.addEventListener("click", openCardPopup);
 userPopupCloseButton.addEventListener("click", closeUserPopup);
 cardPopupCloseButton.addEventListener("click", closeCardPopup);
 formElement.addEventListener("submit", savePopup);
+
+
