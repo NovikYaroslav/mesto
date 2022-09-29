e8d8d179066f050bfd2705b913d4a0ef3d402a48
Yaroslav Novik <yaroslavnowik@yandex.ru>
2022-09-29 20:13:59 +0300

    feat: popups open/close functions refactored

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,49 @@
 const userPopupOpenButton = document.querySelector(".profile__edit-button");
 const cardPopupOpenButton = document.querySelector(".profile__add-button");
-
 const userPopup = document.getElementById("userPopup");
 const cardPopup = document.getElementById("cardPopup");
-
 const userPopupCloseButton = document.getElementById("userPopupClose");
 const cardPopupCloseButton = document.getElementById("cardPopupClose");
-
-const LikeButtons = document.querySelectorAll(".element__like-button")
-
-
+const likeButtons = document.querySelectorAll(".element__like-button");
 const formElement = userPopup.querySelector(".popup__form");
 const nameInput = formElement.querySelector(".user-discription__input_value_name");
 const aboutInput = formElement.querySelector(".user-discription__input_value_about");
 const userName = document.querySelector(".profile__name");
 const aboutUser = document.querySelector(".profile__discription");
 
-
-function openUserPopup() {
-  userPopup.classList.add("popup_opened");
-  nameInput.value = userName.textContent;
-  aboutInput.value = aboutUser.textContent;
+function openPopup(event) {
+  if (event.target === userPopupOpenButton) {
+    userPopup.classList.add("popup_opened");
+    nameInput.value = userName.textContent;
+    aboutInput.value = aboutUser.textContent;
+  }
+  if (event.target === cardPopupOpenButton) {
+    cardPopup.classList.add("popup_opened");
+  }
 }
-function openCardPopup() {
-  cardPopup.classList.add("popup_opened");
-}
 
-function closeUserPopup() {
+function closePopup() {
   userPopup.classList.remove("popup_opened");
-}
-function closeCardPopup() {
   cardPopup.classList.remove("popup_opened");
 }
-//сделать eventTarget
 
 function savePopup(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   aboutUser.textContent = aboutInput.value;
-  closeUserPopup();
+  closePopup();
 }
 
 function likeCard(event) {
-  event.target.classList.toggle("element__like-button_active")
+  event.target.classList.toggle("element__like-button_active");
 }
 
-LikeButtons.forEach(function (likeButton) {
-  likeButton.addEventListener("click", likeCard)
-})
+likeButtons.forEach(function (likeButton) {
+  likeButton.addEventListener("click", likeCard);
+});
 
-userPopupOpenButton.addEventListener("click", openUserPopup);
-cardPopupOpenButton.addEventListener("click", openCardPopup);
-userPopupCloseButton.addEventListener("click", closeUserPopup);
-cardPopupCloseButton.addEventListener("click", closeCardPopup);
+userPopupOpenButton.addEventListener("click", openPopup);
+cardPopupOpenButton.addEventListener("click", openPopup);
+userPopupCloseButton.addEventListener("click", closePopup);
+cardPopupCloseButton.addEventListener("click", closePopup);
 formElement.addEventListener("submit", savePopup);
-
-
