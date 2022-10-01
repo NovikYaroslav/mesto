a748ac36a2e9dc3915e232ff2deb3411c5c70c5a
Yaroslav Novik <yaroslavnowik@yandex.ru>
2022-10-01 14:07:44 +0300

    fieldsets classes refactored

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
-
 const userPopupOpenButton = document.querySelector(".profile__edit-button");
 const cardPopupOpenButton = document.querySelector(".profile__add-button");
 const cardsPhotos = document.querySelectorAll(".element__photo");
@@ -41,15 +40,18 @@ const likeButtons = document.querySelectorAll(".element__like-button");
 const likeButtonsArray = Array.from(likeButtons);
 const formElement = userPopup.querySelector(".popup__form");
 const nameInput = formElement.querySelector(
-  ".user-discription__input_value_name"
+  ".popup-fieldset__input_value_name"
 );
 const aboutInput = formElement.querySelector(
-  ".user-discription__input_value_about"
+  ".popup-fieldset__input_value_about"
 );
 const userName = document.querySelector(".profile__name");
 const aboutUser = document.querySelector(".profile__discription");
 const deleteButtons = document.querySelectorAll(".element__delete-button");
 
+// Функции
+
+// Функция, открытия Попаво профиля и карточки.
 function openPopup(event) {
   if (event.target === userPopupOpenButton) {
     userPopup.classList.add("popup_opened");
@@ -61,9 +63,13 @@ function openPopup(event) {
   }
 }
 
+// Функция закрытия Попапов
+
 function closePopup(currentPopup) {
   currentPopup.classList.remove("popup_opened");
 }
+
+// Функция сохранения Попапа профиля
 
 function savePopup(evt) {
   evt.preventDefault();
@@ -72,39 +78,52 @@ function savePopup(evt) {
   closePopup(userPopup);
 }
 
+// Функция активации лайка
 function likeCard(event) {
   event.target.classList.toggle("element__like-button_active");
 }
 
-likeButtonsArray.forEach(function (likeButton) {
-  likeButton.addEventListener("click", likeCard);
-});
-
+// Функция открытия Попапа фотографии
 function openPhoto(event) {
   photoPopup.classList.toggle("popup_opened");
   popupPhoto.src = event.target.src;
   popupPhotoCapture.textContent = "Подпись из карточки";
 }
 
+// Функция удаления карточки
+function deleteCard(event) {
+  event.target.parentElement.remove();
+}
+
+//Обработка массивов
+
+// Обработка массива с кнопками лайков
+likeButtonsArray.forEach(function (likeButton) {
+  likeButton.addEventListener("click", likeCard);
+});
+
+// Обработка ноды с карточками
 cardsPhotos.forEach(function (cardPhoto) {
   cardPhoto.addEventListener("click", openPhoto);
 });
 
-function deleteCard(event) {
-event.target.parentElement.remove();
-}
-
+// обработка ноды с кнопками удаления
 deleteButtons.forEach(function (deleteButton) {
-  deleteButton.addEventListener("click", deleteCard)
-})
+  deleteButton.addEventListener("click", deleteCard);
+});
 
+// Слушатели
 userPopupOpenButton.addEventListener("click", openPopup);
 cardPopupOpenButton.addEventListener("click", openPopup);
 
-
-userPopupCloseButton.addEventListener("click", function () {closePopup(userPopup)});
-cardPopupCloseButton.addEventListener("click", function () {closePopup(cardPopup)});
-photoPopupCloseButton.addEventListener("click", function () {closePopup(photoPopup)});
-
+userPopupCloseButton.addEventListener("click", function () {
+  closePopup(userPopup);
+});
+cardPopupCloseButton.addEventListener("click", function () {
+  closePopup(cardPopup);
+});
+photoPopupCloseButton.addEventListener("click", function () {
+  closePopup(photoPopup);
+});
 
 formElement.addEventListener("submit", savePopup);
